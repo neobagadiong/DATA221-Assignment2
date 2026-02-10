342aed072b2ab5b4d2e147eb803cd4718fdd0f10
neobagadiong <neo.bagadiong02@gmail.com>
2026-02-10 13:04:07 -0700

    Q3 code added

---
+++ Question3.py
@@ -0,0 +1,23 @@
+import string
+inputTextFile = open("sample-file.txt")
+normalizedLineDictionary ={}
+normalizedLineList = []
+
+
+for line in inputTextFile:
+    normalizedLine = ''.join(line.translate(str.maketrans('', '', string.punctuation)).lower().split())
+    if normalizedLine != '':
+        normalizedLineList.append(normalizedLine) 
+        
+for index in range(len(normalizedLineList)):
+        if normalizedLineList[index] in normalizedLineDictionary.keys():
+            normalizedLineDictionary[normalizedLineList[index]].append(index)
+        else:
+            normalizedLineDictionary[normalizedLineList[index]] = [index]
+
+for key,values in normalizedLineDictionary.items():
+    if len(values) >= 2:
+        print(key,values)
+
+
+
