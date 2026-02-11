e3100497507dce559db64fe9f393f5c47ae271e5
neobagadiong <neo.bagadiong02@gmail.com>
2026-02-10 22:53:19 -0700

    added q4 and q5

---
+++ Question5.py
@@ -0,0 +1,20 @@
+import pandas 
+import numpy
+studentStatsDataFrame = pandas.read_csv('student.csv')
+
+
+studentStatsDataFrame['grade_band'] = numpy.select([(studentStatsDataFrame['grade'] >= 15),(studentStatsDataFrame['grade'] >= 10),(studentStatsDataFrame['grade'] < 10)],['High','Medium','Low'])
+
+print (studentStatsDataFrame)
+
+studentStatsSummaryDataFrame = pandas.DataFrame(index=studentStatsDataFrame['grade_band'].unique(),columns=['student_count','average_absences','internet_access'])
+
+#print(len(studentStatsDataFrame[studentStatsDataFrame['grade_band'] == 'Medium'].index))
+
+for gradeBand in studentStatsSummaryDataFrame.index:
+    studentStatsSummaryDataFrame.at[gradeBand,'student_count'] = len(studentStatsDataFrame[studentStatsDataFrame['grade_band'] == gradeBand].index)
+    studentStatsSummaryDataFrame.at[gradeBand,'average_absences'] = round(studentStatsDataFrame[studentStatsDataFrame['grade_band'] == gradeBand]['absences'].mean(),2)
+    studentStatsSummaryDataFrame.at[gradeBand,'internet_access'] = round(studentStatsDataFrame[studentStatsDataFrame['grade_band'] == gradeBand]['internet'].mean(),2)
+
+    
+print(studentStatsSummaryDataFrame)
