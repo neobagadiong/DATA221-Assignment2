76ac7dae10ec952e08fbca5563e2bfe42c772fae
neobagadiong <neo.bagadiong02@gmail.com>
2026-02-11 11:13:45 -0700

    q9

--- Question9.py
+++ Question9.py
@@ -5,11 +5,15 @@ import requests, csv
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 OPR/126.0.0.0 (Edition std-1)',
 }
-url = 'https://en.wikipedia.org/wiki/Machine_learning'z
+url = 'https://en.wikipedia.org/wiki/Machine_learning'
 
 machineLearningWikiHTML = BeautifulSoup(requests.get(url, headers=header).text , 'html.parser')
 
-rosterTable = machineLearningWikiHTML.find(id='mw-content-text').find_('table')
+machineLarning = machineLearningWikiHTML.find(id='mw-content-text').find_('table')
+
+
+
+
 
 print(rosterTable)
 
