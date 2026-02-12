12cdc44571b6bc8cce30b4f13cfd3cc2fdf21e51
neobagadiong <neo.bagadiong02@gmail.com>
2026-02-11 19:07:19 -0700

    Q4, Q5, Q9 added

--- Question5.py
+++ Question5.py
@@ -1,20 +1,20 @@
 import pandas 
 import numpy
 studentStatsDataFrame = pandas.read_csv('student.csv')
+#print (studentStatsDataFrame)
 
+studentStatsDataFrame['grade_band'] = numpy.select([(studentStatsDataFrame['grade'] >= 15),(studentStatsDataFrame['grade'] >= 10),(studentStatsDataFrame['grade'] <= 9)],['High','Medium','Low'],default='None')
 
-studentStatsDataFrame['grade_band'] = numpy.select([(studentStatsDataFrame['grade'] >= 15),(studentStatsDataFrame['grade'] >= 10),(studentStatsDataFrame['grade'] < 10)],['High','Medium','Low'])
-
-print (studentStatsDataFrame)
+#print (studentStatsDataFrame)
 
 studentStatsSummaryDataFrame = pandas.DataFrame(index=studentStatsDataFrame['grade_band'].unique(),columns=['student_count','average_absences','internet_access'])
 
-#print(len(studentStatsDataFrame[studentStatsDataFrame['grade_band'] == 'Medium'].index))
+print(len(studentStatsDataFrame[studentStatsDataFrame['grade_band'] == 'None'].index))
 
 for gradeBand in studentStatsSummaryDataFrame.index:
     studentStatsSummaryDataFrame.at[gradeBand,'student_count'] = len(studentStatsDataFrame[studentStatsDataFrame['grade_band'] == gradeBand].index)
     studentStatsSummaryDataFrame.at[gradeBand,'average_absences'] = round(studentStatsDataFrame[studentStatsDataFrame['grade_band'] == gradeBand]['absences'].mean(),2)
     studentStatsSummaryDataFrame.at[gradeBand,'internet_access'] = round(studentStatsDataFrame[studentStatsDataFrame['grade_band'] == gradeBand]['internet'].mean(),2)
 
-    
-print(studentStatsSummaryDataFrame)
+studentStatsSummaryDataFrame.index
+studentStatsSummaryDataFrame.to_csv('student_bands.csv',index_label='')
--- Question9.py
+++ Question9.py
@@ -1,19 +1,43 @@
 from bs4 import BeautifulSoup
 import requests, csv
-
+import pandas
 
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 OPR/126.0.0.0 (Edition std-1)',
 }
 url = 'https://en.wikipedia.org/wiki/Machine_learning'
 
+
 machineLearningWikiHTML = BeautifulSoup(requests.get(url, headers=header).text , 'html.parser')
 
-machineLarning = machineLearningWikiHTML.find(id='mw-content-text').find_('table')
-
-
-
-
+rosterTable = machineLearningWikiHTML.find('div',id='mw-content-text').find_all('table')
 
 print(rosterTable)
+
+firstThreeColumnTable = None
+
+for table in rosterTable:
+    firstRow = table.find('ul')
+
+    columns = firstRow.find_all(['th','td','li'])
+
+    if len(columns) >= 3:
+        firstThreeColumnTable = table
+        break
+
+
+
+HTMLtableToList =[]
+
+for row in firstThreeColumnTable.find_all('tr'):
+    
+    rowText = row.get_text().replace('\n',' ')
+    
+    if rowText[0] == ' ': 
+        HTMLtableToList.append(rowText[1:])
+    else:
+        HTMLtableToList.append(rowText)
+
+wikiHTMLDataFrame = pandas.DataFrame(HTMLtableToList)
+wikiHTMLDataFrame.to_csv('wiki_table.csv',index=False,header=False)
 
